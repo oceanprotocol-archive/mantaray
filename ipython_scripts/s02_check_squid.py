681b7d0924a9256b0bde3376cea73230840b2ccc
MarcusJones <jones.0bj3@gmail.com>
2018-10-01 17:02:03 +0200

    add Docker ps check. Not working yet.

--- ipython_scripts/s02_check_squid.py
+++ ipython_scripts/s02_check_squid.py
@@ -1,4 +1,5 @@
 #%% Imports
+import subprocess
 import pathlib
 import squid_py.ocean as ocean
 import sys
@@ -15,8 +16,10 @@ logger.setLevel(logging.DEBUG)
 
 
 # FORMAT = "%(asctime)s - %(levelno)s - %(module)-15s - %(funcName)-15s - %(message)s"
-FORMAT = "%(asctime)s L%(levelno)s: %(module)30s %(message)s"
+# FORMAT = "%(asctime)s %(levelno)s: %(module)30s %(message)s"
+FORMAT = "%(levelno)s - %(module)-15s - %(funcName)-15s - %(message)s"
 
+DATE_FMT = "%Y-%m-%d %H:%M:%S"
 DATE_FMT = "%Y-%m-%d %H:%M:%S"
 formatter = logging.Formatter(FORMAT, DATE_FMT)
 
@@ -27,9 +30,16 @@ logger.handlers = [handler]
 logger.critical("Logging started")
 
 logger.debug("Logging started")
+
+#%% Check docker
+s = subprocess.check_output('docker ps', shell=True)
+print(s)
+
 #%% Instantiate the wrapper
 PATH_CONFIG = pathlib.Path.cwd() / 'config_local.ini'
 assert PATH_CONFIG.exists(), "{} does not exist".format(PATH_CONFIG)
+
+
 ocn = ocean.Ocean(host='http://0.0.0.0', port=8545, config_path=PATH_CONFIG)
 
 logging.info("Ocean smart contract node connected at {}".format(ocn.node_uri))
@@ -38,3 +48,6 @@ logging.info("{:>40} {}".format("Authentication contract atddress:", ocn.auth.ad
 logging.info("{:>40} {}".format("Market contract address:", ocn.market.address))
 
 logging.info("Metadata store (provider) located at: {}".format(ocn.metadata.base_url))
+
+
+
