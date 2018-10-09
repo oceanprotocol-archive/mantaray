d4075e6e937a91e3f1079aa9ae682ba7dde4c15d
MarcusJones <jones.0bj3@gmail.com>
2018-10-09 15:23:03 +0200

    documentation

--- ipython_scripts/s00_overview.py
+++ ipython_scripts/s00_overview.py
@@ -1,28 +1,66 @@
+# %% [markdown]
+
+# The Mantaray scripts provide a guided tour of Ocean Protocol in an interactive environment.
+#
+# The scripts are designed to be executed in an 'IPython' interactive console.
+#
+# The popular Jupyter Notebook format is supported.
+#
+# Alternatively, you can run the scripts in other IPython environments.
+#
+# The scripts are generated in two formats: .py scripts with the # %% cell demarcation convention, or the auto-converted .ipynb format for i.e. Jupyter Notebook.
+#
+# These scripts also serve as a catalog of building blocks for building more complex user stories and applications.
+
+# %% [markdown]
+# Jupyter Lab can use different python environments (kernels)
+# For easiest use, install the `ipykernel` package into your environment:
+#
+# `pip install ipykernel` or `conda install ipykernel`
+#
+# Then, install the kernel using the IPython command; `ipython kernel install --user --name=projectname`
+#
+# TODO: Check this error in ipykernel deps
+#
+#  If error on above command, try `pip install 'prompt-toolkit==1.0.15'`
+# Then, `python -m ipykernel install --user --name mantaray3 --display-name "Python (mantaray3)" `
+
+# %% [markdown]
+# ## Meanwhile, on the dry dock...
+
 # %%
+# The AWS SDK
+import boto3
+print( "boto3", boto3.__version__ )
 
-"""
+# The docker python utility can be used to manage your local docker configuration
+import docker
+print("docker", docker.__version__)
 
-The Mantaray scripts provide a guided tour of Ocean Protocol in an interactive environment.
+# Alternatively, you may connect to the Kubernetes cluster for testing
+# TODO: k8s util check
 
-The scripts are designed to be executed in an 'IPython' interactive console. The popular Jupyter Notebook format is
-supported. Alternatively, you can run the scripts in other IPython environments. The scripts are generated in two
-formats: .py scripts with the # %% cell demarcation convention, or the auto-converted .ipynb format for i.e. Jupyter Notebook.
+# Or, you can connect to the Ethereum test net
+# TODO: test net util check
 
-These scripts also serve as a catalog of building blocks for building more complex user stories and applications.
+# squid-py is your main interface to Ocean Protocol
+import squid_py
+print("squid_py", squid_py.__version__)
 
-"""
 
-# %% Meanwhile, on the dry dock
 
+# %%
 # Rank:
 # Ensign -
 # Able seaman
 # Before embarking across Ocean Protocol, let's get ship-shape with the following pre-requisites;
 # Pre
 
+# %% [markdown]
+#
+# Ready the sails!
 
 
-# %% Ready the sails!
-
-
-# %% Inspection
+# %% [markdown]
+#
+# Inspection
