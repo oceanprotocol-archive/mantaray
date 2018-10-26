5ec38fe6d0797b76b85842ea3c55a53c70fbe749
MarcusJones <jones.0bj3@gmail.com>
2018-10-26 13:50:03 +0200

    refactor Conversion script updated for subdirs

--- scripts/nb_convert_jupytext.py
+++ scripts/nb_convert_jupytext.py
@@ -13,25 +13,21 @@ path_ipy_root=root_dir.joinpath("../ipython_scripts")
 path_ipy_root=root_dir.joinpath("ipython_scripts")
 path_jupyter_root=root_dir.joinpath("jupyter_notebooks")
 
-for script_dir in path_ipy_scripts.glob('*'):
+for script_dir in path_ipy_root.glob('*'):
     dir_name = script_dir.parts[-1]
+    out_dir = path_jupyter_root.joinpath(dir_name)
+    out_dir.mkdir(parents=True, exist_ok=True)
+    for script_path in script_dir.glob('*.py'):
+        script_fname = script_path.stem + '.ipynb'
+        out_path = out_dir.joinpath(script_fname)
 
-    out_dir =
-    for script in script_dir.glob('*.py'):
-        print(script)
-for py_path in list(path_ipy_scripts.glob('*.py')):
-    # print(py_path)
+        # Read the script, and parse it into memory
+        with script_path.open() as fin:
+            data = fin.read()
+            parsed = jupytext.reads(data, ext='.py', format_name='percent')
 
-    with py_path.open() as fin:
-        data = fin.read()
-        parsed = jupytext.reads(data, ext='.py', format_name='percent')
+        jupytext.writef(parsed, out_path)
 
-    OUTPUT_IPYNB = path_jupyter_nbs / Path(str(py_path.stem) + '.ipynb')
-
-    # assert OUTPUT_IPYNB.exists()
-    # print(OUTPUT_IPYNB)
-    jupytext.writef(parsed, OUTPUT_IPYNB)
-
-    print("Converted {} to .ipynb format".format(py_path.stem))
+        print("Converted {} to .ipynb format".format(out_path.stem))
 
 
