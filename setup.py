b38ab1ac2c16d59a16132722ae509859ef82b4fc
MarcusJones <jones.0bj3@gmail.com>
2018-11-26 12:29:33 +0100

    update Travis

--- setup.py
+++ setup.py
@@ -11,11 +11,29 @@ with open('README.md') as readme_file:
 with open('CHANGELOG.md') as changelog_file:
     changelog = changelog_file.read()
 
-requirements = ['oceandb-driver-interface', ]
+# Core requirements for end-users
+requirements = [
+    'squid-py==0.2.9'
+]
 
-setup_requirements = ['pytest-runner', ]
+# Required packages for developers
+dev_requirements = [
+    'bumpversion',
+    #'pkginfo',
+    'twine',
+    #'watchdog',
+]
 
-test_requirements = ['pytest', ]
+# Extra requirements for setup
+setup_requirements = []
+
+# Requirements for testing
+test_requirements = []
+
+docs_requirements = [
+    'Sphinx',
+    'sphinx-rtd-theme',
+]
 
 setup(
     author="leucothia",
@@ -25,15 +43,17 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    description="Python client library for Ocean Protocol",
+    description="Data Science interface to the Ocean Protocol stack",
+    extras_require={
+        'test': test_requirements,
+        'dev': dev_requirements + test_requirements + docs_requirements,
+    },
     install_requires=requirements,
     license="Apache Software License 2.0",
     long_description=readme,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='mantaray',
     name='mantaray',
@@ -41,7 +61,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/oceanprotocol/oceandb-elasticsearch-driver',
-    version='0.2.0',
+    url='https://github.com/oceanprotocol/mantaray',
+    version='0.0.1',
     zip_safe=False,
 )
