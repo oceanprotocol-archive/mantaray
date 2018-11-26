2e9a615421b68bf83f0553a27c2cebe84a0a7afa
MarcusJones <jones.0bj3@gmail.com>
2018-11-26 14:51:04 +0100

    fix pytest for ocean instance

--- tests/test_ocean.py
+++ tests/test_ocean.py
@@ -1,14 +1,19 @@
 """
 Ensure we can instantiate the Ocean object
 """
+from squid_py.ocean import ocean
+import os
 
-def test_ocean_instance(ocean_instance):
-    ocean_instance.print_config()
-    assert ocean_instance.keeper.token is not None
+def test_ocean_instance():
+    path_config = 'config_local.ini'
+    os.environ['CONFIG_FILE'] = path_config
+    ocn = ocean.Ocean(os.environ['CONFIG_FILE'])
+
+    assert ocn.keeper.token is not None
 
     # There is ONE Web3 instance
-    assert ocean_instance.keeper.market.web3 is ocean_instance.keeper.auth.web3 is ocean_instance.keeper.token.web3
-    assert ocean_instance._web3 is ocean_instance.keeper.web3
+    assert ocn.keeper.market.web3 is ocn.keeper.auth.web3 is ocn.keeper.token.web3
+    assert ocn._web3 is ocn.keeper.web3
 
-    ocean_instance.print_config()
+    ocn.print_config()
 
