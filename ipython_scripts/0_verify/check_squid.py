d9ae661037867ceb49b40b7ed91a97a6bbbc45bd
MarcusJones <jones.0bj3@gmail.com>
2018-11-28 17:01:42 +0100

    refactor Moving to fixtures

--- ipython_scripts/0_verify/check_squid.py
+++ ipython_scripts/0_verify/check_squid.py
@@ -12,30 +12,7 @@ from squid_py.ocean.ocean import Ocean
 import sys
 # from squid_py.utils.web3_helper import convert_to_bytes, convert_to_string, convert_to_text
 
-# %% Logging
-import logging
 
-loggers_dict = logging.Logger.manager.loggerDict
-
-logger = logging.getLogger()
-logger.handlers = []
-
-# Set level
-logger.setLevel(logging.DEBUG)
-
-# FORMAT = "%(asctime)s - %(levelno)s - %(module)-15s - %(funcName)-15s - %(message)s"
-# FORMAT = "%(asctime)s %(levelno)s: %(module)30s %(message)s"
-FORMAT = "%(levelno)s - %(module)-15s - %(funcName)-15s - %(message)s"
-
-DATE_FMT = "%Y-%m-%d %H:%M:%S"
-DATE_FMT = "%Y-%m-%d %H:%M:%S"
-formatter = logging.Formatter(FORMAT, DATE_FMT)
-
-# Create handler and assign
-handler = logging.StreamHandler(sys.stderr)
-handler.setFormatter(formatter)
-logger.handlers = [handler]
-logger.debug("Logging started")
 
 # %% Instantiate the wrapper
 
