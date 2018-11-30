12f93acca4bcb540402b4b02bd6e1b974c29877e
MarcusJones <jones.0bj3@gmail.com>
2018-11-30 14:51:43 +0100

    stub Search Assets

---
+++ ipython_scripts/1_blocks/s00_search_assets.py
@@ -0,0 +1,46 @@
+# %% [markdown]
+# ## Building Blocks: Searching and listing registered assets
+# In this notebook, TODO: description
+
+# %% [markdown]
+# ### Section 0: Housekeeping, import modules, and setup logging
+#%%
+import pathlib
+import sys
+import logging
+from pathlib import Path
+import squid_py
+from squid_py.ocean.ocean import Ocean
+
+# Add the local utilities package
+utilities_path = Path('.') / 'script_fixtures'
+if not utilities_path.exists():
+    utilities_path = Path('.') / '..' / '..' / 'script_fixtures'
+assert utilities_path.exists()
+
+#Get the project root path
+PATH_PROJECT_ROOT = utilities_path / '..'
+PATH_PROJECT_ROOT.absolute()
+
+utilities_path_str = str(utilities_path.absolute())
+if utilities_path_str not in sys.path:
+    sys.path.append(utilities_path_str)
+
+import script_fixtures.logging as util_logging
+util_logging.logger.setLevel('INFO')
+
+import script_fixtures.user as user
+logging.info("Squid API version: {}".format(squid_py.__version__))
+
+# %% [markdown]
+# ### Section 1: Assets in the MetaData store (Aquarius)
+# Anyone can search assets in the public metadata stores
+#%%
+# The contract addresses are loaded from file
+PATH_CONFIG = pathlib.Path.cwd() / 'config_local.ini'
+assert PATH_CONFIG.exists(), "{} does not exist".format(PATH_CONFIG)
+
+ocn = Ocean(config_file=PATH_CONFIG)
+#%%
+ocn.search_assets()
+
