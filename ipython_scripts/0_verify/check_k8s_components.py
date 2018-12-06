3de0db392f18c56824fb29d47a331f8c7666d401
MarcusJones <jones.0bj3@gmail.com>
2018-12-06 15:21:38 +0100

    cleanup Docstrings and comments

--- ipython_scripts/0_verify/check_k8s_components.py
+++ ipython_scripts/0_verify/check_k8s_components.py
@@ -1,6 +1,5 @@
 # %%
 import requests
-import sys
 import os
 import logging
 
@@ -10,43 +9,38 @@ import mantaray_utilities.config as manta_config
 import mantaray_utilities.logging as manta_logging
 
 #%%
-# For this test, set the configuration environment variable
+# For this test, set the configuration environment variable for kubernetes.
+# here it is hard-coded for IPython execution, but in general, it is set in your system environment.
+manta_config.name_deployment_type()
 os.environ['USE_K8S_CLUSTER'] = 'true'
 manta_config.name_deployment_type()
 
-# When running in IPython, ensure the project path is correct
-# This may vary according to your environment
 manta_logging.logger.setLevel('INFO')
 
-# Get the configuration file path
+# Get the configuration file path for this environment
 CONFIG_INI_PATH = manta_config.get_config_file_path()
-# manta_config.get_project_path() /
-# Add the local utilities package
-# utilities_path = PATH_PROJECT / 'script_fixtures'
-# assert utilities_path.exists()
-# utilities_path = str(utilities_path.absolute())
-# if utilities_path not in sys.path:
-#     sys.path.append(utilities_path)
-
-
+logging.info("Configuration file selected: {}".format(CONFIG_INI_PATH))
 
 #%%
 
+# For now, the endpoints are hard-coded by the dev-ops team.
 endpoints_dict = {
     'keeper-contracts': 'http://52.1.94.55:8545',
     'pleuston': 'http://ac98d76bade8d11e89c320e965e714bc-981020006.us-east-1.elb.amazonaws.com:3000/',
     'aquarius': 'http://ac8b5e618ef0511e88a360a98afc4587-575519081.us-east-1.elb.amazonaws.com:5000',
     'brizo': 'http://ac8b8cc42ef0511e88a360a98afc4587-974193642.us-east-1.elb.amazonaws.com:8030',
     'secret_store' : 'https://secret-store.dev-ocean.com'
-
 }
 endpoints_dict['aquarius Swagger documentation'] = endpoints_dict['aquarius'] + '/api/v1/docs/'
 endpoints_dict['brizo Swagger documentation'] = endpoints_dict['brizo'] + '/api/v1/docs/'
+
 def check_endpoint(endpoint_name, endpoint_url, verb='GET', ):
+    """HTTP Request on the given URL"""
     res = requests.request(verb, endpoint_url)
     logging.debug("{} : returns {}".format(endpoint_name, res.status_code))
     return res.status_code, res.content
 
+# Iterate over the defined endpoints
 for endpoint in endpoints_dict:
     with manta_logging.LoggerCritical():
         print("Checking {}".format(endpoint))
@@ -56,18 +50,9 @@ for endpoint in endpoints_dict:
         except:
             print('\t Failed!')
 
-# check_endpoint('aquarius_doc', endpoints_dict)
-# check_endpoint('aquarius', endpoints_dict)
-# check_endpoint('keeper-contracts', endpoints_dict)
-# check_endpoint('pleuston', endpoints_dict)
-# check_endpoint('brizo', endpoints_dict)
-# check_endpoint('brizo_doc', endpoints_dict)
-
 #%%
-
+# Finally, attempt to instantiate the Ocean API with the configured endpoints
 with manta_logging.LoggerCritical():
     ocn = ocean.Ocean(CONFIG_INI_PATH)
 print("*******************")
 print("Ocean successfully instantiated with kubernetes!")
-
-
