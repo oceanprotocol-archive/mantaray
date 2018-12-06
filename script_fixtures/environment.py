f93bbddd698c4341bd948d0af88d1327083fecad
MarcusJones <jones.0bj3@gmail.com>
2018-12-06 10:52:17 +0100

    add Utility to detect running env

---
+++ script_fixtures/environment.py
@@ -0,0 +1,10 @@
+import os
+import logging
+
+def jupyter_env():
+    if 'JUPYTER_DEPLOYMENT' in os.environ:
+        logging.info("Jupyter Hub deployement detected.".format())
+        return True
+    else:
+        logging.info("Default deployment detected.".format())
+        return False
