b2f36f31cbed405cb1cdd28463e0b64d31f8e1c9
MarcusJones <jones.0bj3@gmail.com>
2018-12-10 15:55:08 +0100

    add Selector for env deployment variable

--- ipython_scripts/0_notebooks_verify/check_squid.py
+++ ipython_scripts/0_notebooks_verify/check_squid.py
@@ -5,7 +5,7 @@
 #%%
 # Standard imports
 import logging
-
+import os
 # Import mantaray and the Ocean API (squid)
 from squid_py.ocean.ocean import Ocean
 import mantaray_utilities.config as manta_config
@@ -13,6 +13,8 @@ import mantaray_utilities.logging as manta_logging
 import mantaray_utilities.asset_pretty_print as manta_print
 #%%
 # Get the configuration file path for this environment
+# os.environ['USE_K8S_CLUSTER']='true'
+logging.info("Deployment type: {}".format(manta_config.get_deployment_type()))
 CONFIG_INI_PATH = manta_config.get_config_file_path()
 logging.info("Configuration file selected: {}".format(CONFIG_INI_PATH))
 
