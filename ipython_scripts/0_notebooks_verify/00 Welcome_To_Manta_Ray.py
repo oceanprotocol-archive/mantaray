d9927728c543ddcedf8076ed18b137d299980b48
MarcusJones <jones.0bj3@gmail.com>
2018-12-17 09:44:01 +0100

    update Docs

--- ipython_scripts/0_notebooks_verify/00 Welcome_To_Manta_Ray.py
+++ ipython_scripts/0_notebooks_verify/00 Welcome_To_Manta_Ray.py
@@ -2,22 +2,29 @@
 # # Welcome to Manta Ray
 # ## Data Science powered by Ocean Protocol
 #
-# The **Manta Ray** scripts provide a guided tour of Ocean Protocol in an interactive Jupyter Notebook environment.
+# The **Manta Ray** notebooks provide a guided tour of Ocean Protocol in an interactive JupyterLab environment.
 #
-# Scripts are first written in regular Python (with the `#%%` cell demarcation formatting) in the
-# [mantaray](https://github.com/oceanprotocol/mantaray) repo, and then converted into Jupyter Notebooks, `.ipynb`.
-# A seperate GitHub repository holds these converted notebooks, to provide an uncluttered directory for JupyterHub. This repo,
-# [mantaray_jupyter](https://github.com/oceanprotocol/mantaray_utilities), is then cloned into the environment you are using.
+# Scripts are first written in regular Python (with the `#%%` cell demarcation format). You see the source
+# [mantaray](https://github.com/oceanprotocol/mantaray) Github repository. These scripts are then converted into Jupyter Notebooks, `.ipynb`.
+# A seperate GitHub repository holds these converted notebooks, to provide an uncluttered directory for the JupyterHub server. This repo,
+# [mantaray_jupyter](https://github.com/oceanprotocol/mantaray_utilities), is then cloned into the environment you are currently using.
 #
-# As a Data Scientist, you may feel free to copy these notebooks or parts thereof to build a new Notebook. Or if you
-# prefer to work in pure python, please go to the mantaray source repo and review the python scripts in the `/ipython` directory.
+# As a Data Scientist or Data Engineer, you may wish to copy these notebooks or parts thereof to build a new notebook for your pipeline. Or if you
+# prefer to work in pure Python, please go to the mantaray source repo and review the `.py` scripts in the `/ipython` directory.
 #
-#%% [markdown]
-# ### For developers:
-# Cloud components are used to simulate Ocean Protocol. Local docker components are [also available](https://github.com/oceanprotocol/docker-images).
+# As a Developer, you can use the notebooks as a tutorial and demonstration in working with Ocean Protocol.
 #
-# The main import is the [squid-py](https://github.com/oceanprotocol/squid-py) ([PyPI link](https://pypi.org/project/squid-py/))
-# Ocean Python API. This in-turn depends on downloading the Application Binary Interface files for the deployed Ethereum smart contracts. These ABI files are
+# %% [markdown]
+# Notes on architecture:
+#
+# The Ocean Protocol stack is deployed to the cloud to simulate the blockchain and middle-ware.
+#
+# Local docker components are [also available](https://github.com/oceanprotocol/docker-images) for developers.
+#
+# The primary imported library is called [squid-py](https://github.com/oceanprotocol/squid-py) ([PyPI link](https://pypi.org/project/squid-py/)), the
+# Ocean Protocol Python API. Squid is your interface to the deployed smart contracts.
+#
+# This in-turn depends on downloading the Application Binary Interface files for the deployed Ethereum smart contracts. These ABI files are
 # deployed to PyPI as the [keeper-contracts](https://pypi.org/project/keeper-contracts/) package.
 #
 # Instructions for setting a developer environment are available at our documentation website TODO: (releasing soon!)
