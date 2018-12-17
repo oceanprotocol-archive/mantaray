dcc5c3e55acde8b46eb8470cea685c5b5e09bb6c
MarcusJones <jones.0bj3@gmail.com>
2018-12-17 10:14:16 +0100

    update Intro text

--- ipython_scripts/0_notebooks_verify/00 Welcome_To_Manta_Ray.py
+++ ipython_scripts/0_notebooks_verify/00 Welcome_To_Manta_Ray.py
@@ -17,41 +17,82 @@
 # %% [markdown]
 # Notes on architecture:
 #
+# The present JupyterLab instance is running on your own virtual machine,
+#
 # The Ocean Protocol stack is deployed to the cloud to simulate the blockchain and middle-ware.
+# These endpoints are configured in the `config_jupyter.ini` file.
 #
 # Local docker components are [also available](https://github.com/oceanprotocol/docker-images) for developers.
 #
-# The primary imported library is called [squid-py](https://github.com/oceanprotocol/squid-py) ([PyPI link](https://pypi.org/project/squid-py/)), the
+# The primary imported library is called [squid-py](https://github.com/oceanprotocol/squid-py)
+# ([PyPI link](https://pypi.org/project/squid-py/)), the
 # Ocean Protocol Python API. Squid is your interface to the deployed smart contracts.
 #
-# This in-turn depends on downloading the Application Binary Interface files for the deployed Ethereum smart contracts. These ABI files are
+# This in-turn depends on downloading the Application Binary Interface files for the deployed Ethereum smart contracts.
+# These ABI files are
 # deployed to PyPI as the [keeper-contracts](https://pypi.org/project/keeper-contracts/) package.
 #
 # Instructions for setting a developer environment are available at our documentation website TODO: (releasing soon!)
-
+#
+# For questions, help, bug reports, etc., please contact us in our [Gitter Channel](https://gitter.im/oceanprotocol/Lobby)
 # %% [markdown]
 # ## Meanwhile, on the dry dock...
-# Let's get ship-shape! First, check your dependencies using the cell below. Install any missing components into your environment as necessary.
+# Let's get ship-shape! First, check your dependencies using the cell below. Install any missing components into your
+# environment as necessary.
 
 #%% [markdown]
 # **squid-py** is your friendly interface to **Ocean Protocol**
+
 #%%
 import squid_py
 print("squid_py", squid_py.__version__)
 
 #%% [markdown]
-# An set of [utility methods](https://github.com/oceanprotocol/mantaray_utilities) is installed, for extra functionality for the Data Science community
+# The main class of
+
+#%% [markdown]
+# An set of [utility methods](https://github.com/oceanprotocol/mantaray_utilities) is installed, for extra functionality
+# for the Data Science community.
+
 #%%
 import mantaray_utilities
 print("mantaray_utilities", mantaray_utilities.__version__)
+
+# %% [markdown]
+# Some of the main utilities are the `config` module, and the `user` module, for simulating a User.
+
+#%%
+# The config module makes it easier to manage local and deployed notebooks and components.
+import mantaray_utilities.config
+# The deployment type is discovered using environment variables
+# JUPYTER_DEPLOYMENT    - For JupyterHub/Lab, and deployed cloud components
+# USE_K8S_CLUSTER       - For running scripts locally, but still connecting to the deployed cloud components
+# DEFAULT               - Running scripts locally, and running the components locally in Docker
+
+print("***Deployment***")
+
+# The deployment is retrieved using;
+print("Deployment environment:", mantaray_utilities.config.get_deployment_type())
+
+# Each deployment type has a 'project' root path;
+print("Project path:", mantaray_utilities.config.get_project_path())
+
+# And each deployment type has a default configuration script;
+print("Configuration file:", mantaray_utilities.config.get_config_file_path())
+
 # %% [markdown]
 ## Ship's manifest
 # In the left pane, several scripts are loaded, designed to walk you through the Ocean Protocol stack.
 #
-# First, check the functionality of the cloud components with `check_k8s_components`.
+# First, check the functionality of the cloud components with `check_k8s_components`. This script will iterate and
+# check the status over each of each deployed URL.
 #
-# Then, you can instantiate the main class for interacting with Ocean, the aptly named **Ocean** class in `squid-py`, in the `check_squid` notebook.
+# Then, you can instantiate the main class for interacting with Ocean, the aptly named **Ocean** class in `squid-py`,
+# in the `check_squid` notebook.
 #
-# The next set of notebooks each focus on a main aspect of Ocean Protocol. These can be composed into a complete data science pipeline.
+# The next set of notebooks each focus on a main aspect of Ocean Protocol. These can be composed into a complete data
+# science pipeline.
 #
-# Weigh anchor, and good luck sailing into Ocean Protocol!
+# Weigh anchor, and smooth sailing into Ocean Protocol!
+#
+# For questions, help, bug reports, etc., please contact us in our [Gitter Channel](https://gitter.im/oceanprotocol/Lobby)
