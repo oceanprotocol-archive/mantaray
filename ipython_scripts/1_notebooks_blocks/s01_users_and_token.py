76c7aa196f99aec2602458795ec6aff522884c2a
MarcusJones <jones.0bj3@gmail.com>
2019-01-10 12:50:41 +0100

    refactor Cleanup after new API

--- ipython_scripts/1_notebooks_blocks/s01_users_and_token.py
+++ ipython_scripts/1_notebooks_blocks/s01_users_and_token.py
@@ -34,6 +34,7 @@ logging.info("Squid API version: {}".format(squid_py.__version__))
 from pprint import pprint
 # Setup logging to a higher level and not flood the console with debug messages
 manta_utils.logging.logger.setLevel('CRITICAL')
+from squid_py.keeper.web3_provider import Web3Provider
 
 #%%
 # Get the configuration file path for this environment
@@ -64,92 +65,32 @@ print("Associated password:",configuration['keeper-contracts']['parity.password'
 # %% [markdown]
 # ## Section 2: Instantiate the Ocean API class with this configuration
 # %%
-ocn = Ocean(configuration)
+ocn_user1 = Ocean(configuration)
 logging.critical("Ocean smart contract node connected ".format())
 
+print(len(ocn_user1.accounts), "accounts exist")
 # The Ocean API, during development, queries the blockchain to return all created (simulated) accounts;
-for acct in ocn.accounts:
+for acct in ocn_user1.accounts:
     print(acct)
 
 # Alternatively, the accounts are available on the keeper instance;
-print(ocn.keeper.accounts)
+# print(ocn.keeper.accounts)
 # %% [markdown]
+# ### The User Account creed
+# *this is my account. there are many like it, but this one is mine.
+# my account is my best friend. it is my life. i must master it as i must master my life.
+# without me, my account is useless. without my account, i am useless. i must use my account true. *
+#
 # The Ocean API has a 'main_account', this is the currently active (your) account.
-#%%
-ocn.main_account.address
-
-#%%
-# List the accounts created in the node
-# ocn.accounts is a {address: Account} dict
-print(len(ocn.accounts), "ocean accounts available with following addresses:")
-for address in ocn.accounts:
-    acct = ocn.accounts[address]
-    print(acct.address)
-
-
-#%%
-# The configuration has a 'main account'. This is the currently active and unlocked account.
-
-#%%
-ocn.main_account.address
-# %% [markdown]
-# ### Section 2: From accounts, to Users
 #
-# A simple wrapper for each address is used to represent a user.
-# See: [./script_fixtures/user.py](https://github.com/oceanprotocol/mantaray_utilities/blob/8e3128b49ec8ba00f4f8056a4c888e86b23a5c5c/mantaray_utilities/user.py#L13)
-
-#%% [markdown]
-# Users are instantiated and listed
-#
-# Selected accounts are unlocked via password.
-# A password.csv file should be located in the project root directory, with each row containing <address>,<password>
-#
-# In the following cell, `num_users` specifies how many of the available acocunts will be processed.
-# The script will alternate between Data Scientist and Data Owner roles.
+# It is not secure to send your password over an unsecured HTTP connection, this is for demonstration only!
 #%%
-# Create some simulated users of Ocean Protocol
-# Alternate between Data Scientists (Consumers)
-# and Data Owners (providers)
-users = list()
-num_users = 4
-address_list = [acct for acct in ocn.accounts]
-for i, acct_address in enumerate(address_list[0:num_users]):
-    if i%2 == 0: role = 'Data Scientist'
-    else: role = 'Data Owner'
-    role = "User"
-    if acct_address.lower() in list(PASSWORD_MAP.keys()):
-        this_password = PASSWORD_MAP[acct_address]
-    else:
-        this_password = None
+print("Account address: ", ocn_user1.main_account.address)
+print("Account password: ", ocn_user1.main_account.password)
+print("Account Ether balance: ", ocn_user1.main_account.ether_balance) # TODO: Convert from wei?
+print("Account Ocean Token balance: ", ocn_user1.main_account.ocean_balance)
 
-    user = manta_user.User(names.get_full_name(), role, acct_address, this_password, CONFIG_INI_PATH)
-    users.append(user)
-
-# Select only unlocked accounts
-unlocked_users = [u for u in users if u.credentials]
-logging.info("Selected {} unlocked accounts for simulation.".format(len(unlocked_users)))
-
-#%%
-# (Optional)
-# Delete the configuration files in the /user_configurations folder
-for f in Path('.').glob('user_configurations/*.ini'):
-    f.unlink()
-
-#%% [markdown]
-# List the users
-#%%
-for u in unlocked_users: print(u)
-
-#%% [markdown]
-# ### Section 3: Filling your chest with a bounty of ERC20 token
-# Get these users some Ocean token
-#%%
-for usr in unlocked_users:
-    if usr.account.ocean_balance == 0:
-        rcpt = usr.account.request_tokens(random.randint(0, 100))
-        usr.ocn._web3.eth.waitForTransactionReceipt(rcpt)
-
-#%% [markdown]
-# List the users, and notice the updated balance
-#%%
-for u in unlocked_users: print(u)
+flag_unlocked = Web3Provider.get_web3().personal.unlockAccount(ocn_user1.main_account.address, ocn_user1.main_account.password)
+print("Account unlocked:", flag_unlocked)
+# The following method can be used to lock an account
+#Web3Provider.get_web3().personal.lockAccount(ocn.main_account.address)
