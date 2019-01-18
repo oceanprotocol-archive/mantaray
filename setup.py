dc8b7a296f0b8d890fc5676af5bae8f2947a9034
MarcusJones <jones.0bj3@gmail.com>
2019-01-18 09:23:07 +0100

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('CHANGELOG.md') as changelog_file:
 
 # Core requirements for end-users
 requirements = [
-    'squid-py==0.2.11'
+    'squid-py==0.2.22'
 ]
 
 # Required packages for developers
