b1c9e1489c583d13fff8857dfd1cbcef1820d381
MarcusJones <jones.0bj3@gmail.com>
2019-02-01 13:15:57 +0100

    update

--- ipython_scripts/0_notebooks_verify/check_validator.py
+++ ipython_scripts/0_notebooks_verify/check_validator.py
@@ -4,9 +4,18 @@
 # %%
 ## Imports
 import logging
+from pathlib import Path
 logging.basicConfig(level=logging.DEBUG)
 
-import plecos
-import plecos.plecos
+import plecos.plecos as plecos
+# from plecos import validate
+# import plecos.plecos
+# plecos.validate('asdf')
+# print(plecos)
+# print(plecos.__file__)
+PATH_DATA_ROOT = Path("~/DATA").expanduser()
+path_data_dir = PATH_DATA_ROOT / 'British_birdsong'
+path_metadata = path_data_dir / 'metadata.json'
+plecos.validate(path_metadata)
+# plecos.validate_cli
 
-plecos.validate_cli
