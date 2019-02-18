fd833090a7c2facbfbb56c230eba5905501fd42b
MarcusJones <jones.0bj3@gmail.com>
2019-02-18 15:55:42 +0100

    update Publish custom S3 asset

--- ipython_scripts/1_notebooks_blocks/s0X_publish_custom.py
+++ ipython_scripts/1_notebooks_blocks/s0X_publish_custom.py
@@ -51,81 +51,39 @@ print("Publisher account balance:", ocn.accounts.balance(publisher_acct).ocn)
 if ocn.accounts.balance(publisher_acct).ocn == 0:
     ocn.accounts.request_tokens(publisher_acct, 100)
 
-
-#%% [markdown]
+# %% [markdown] ########################################################################################################
+# GET METADATA
+# ######################################################################################################################
 # %%
 
-this_url = "https://s3.amazonaws.com/datacommons-seeding-us-east/10_Monkey_Species_Small/remote_metadata_clean.json"
+this_url = "https://s3.amazonaws.com/datacommons-seeding-us-east/10_Monkey_Species_Small/metadata_OEP8.json"
 response = requests.get(this_url)
 if response.status_code == 200:
     metadata = response.json()
 else:
     raise
-# pprint(metadata)
 
+pprint(metadata)
 print("Name of Asset:", metadata['base']['name'])
 print("Price of Asset:", metadata['base']['price'])
 
-#%%
 for i, file in enumerate(metadata['base']['files']):
     print("Asset link {}: {}".format( i, file['url']))
 
+# %% [markdown] ########################################################################################################
+# REGISTER
+# ######################################################################################################################
 # %%
 ddo = ocn.assets.create(metadata, publisher_acct)
 registered_did = ddo.did
 print("New asset registered at", registered_did)
-# %% [markdown]
-# Inspect the new DDO. We can retrieve the DDO as a dictionary object, feel free to explore the DDO in the cell below!
-#%%
-ddo_dict = ddo.as_dictionary()
-print("DID:", ddo.did)
-print("Services within this DDO:")
-for svc in ddo_dict['service']:
-    print(svc['type'], svc['serviceEndpoint'])
+
+# %% [markdown] ########################################################################################################
+# CONSUME
+# ######################################################################################################################
+# %%
 
 # %% [markdown]
-# Note that the 'files' attribute has been replaced by the 'encryptedFiles' attribute!
-#%%
-assert 'files' not in ddo.metadata['base']
-print("Encryped 'files' attribute, everything safe and secure!")
-print("Encrypted files decrypt on purchase! [{}...] etc. ".format(ddo.metadata['base']['encryptedFiles'][:50]))
-
-# %% [markdown]
-# ## Section 4: Verify your asset
-# Now, let's verify that this asset exists in the MetaData storage.
 #
-# A call to assets.resolve() will call the Aquarius service and retrieve the DID Document
-#%% {HELLO:test}
-#+attr_jupyter: some cell metadata stuff
-#+attr_jupyter: some other metadata stuff
-
-#TODO: Better handling based on reciept
-print("Wait for the transaction to complete!")
-sleep(10)
-# %%
-ddo = ocn.assets.resolve(registered_did)
-print("Asset '{}' resolved from Aquarius metadata storage: {}".format(ddo.did,ddo.metadata['base']['name']))
-
-# %% [markdown]
-# For illustrative purposes, this is the error you can expect if the DID is *NOT* in the database
-# %%
-random_did = 'did:op:9a3c2693c1f942b8a61cba7d212e5cd50c1b9a5299f74e39848e9b4c2148d453'
-try: ocn.assets.resolve(random_did)
-except Exception as e: print("(This raises, as required)", e)
-
-# %% [markdown]
-# Similarly, we can verify that this asset is registered into the blockchain, and that you are the owner.
-#
-# Congratulations on publishing an Asset into Ocean Protocol!
-
-# %%
-# We need the pure ID string as in the DID registry (a DID without the prefixes)
-asset_id = squid_py.did.did_to_id(registered_did)
-owner = ocn._keeper.did_registry.contract_concise.getOwner(asset_id)
-print("Asset ID", asset_id, "owned by", owner)
-assert str.lower(owner) == str.lower(publisher_acct.address)
-
-# %% [markdown]
-# Next, let's search for our assets in Ocean Protocol
-
+#%%
 
