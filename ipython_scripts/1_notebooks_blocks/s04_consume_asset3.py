a2ae7804fc56a511fe2cbf31c40a15bc6a633dff
MarcusJones <jones.0bj3@gmail.com>
2019-03-21 11:08:52 +0100

    fix Consume works

--- ipython_scripts/1_notebooks_blocks/s04_consume_asset3.py
+++ ipython_scripts/1_notebooks_blocks/s04_consume_asset3.py
@@ -57,70 +57,6 @@ def get_publisher_account(config):
         acc = Account(Keeper.get_instance().accounts[0])
     return acc
 
-#%%
-# from squid_py import Config
-# class ExampleConfig:
-#     environment = 'TEST_NILE'
-#     config_dict = {
-#         "keeper-contracts": {
-#             "keeper.url": "https://nile.dev-ocean.com",
-#             "keeper.path": "artifacts",
-#             "secret_store.url": "https://secret-store.dev-ocean.com",
-#             "parity.url": "https://nile.dev-ocean.com",
-#             "parity.address": "0x413c9ba0a05b8a600899b41b0c62dd661e689354",
-#             "parity.password": "ocean_secret",
-#             "parity.address1": "0x1322A6ef2c560107733bFc622Fe556961Cb430a5",
-#             "parity.password1": "ocean_secret"
-#         },
-#         "resources": {
-#             "aquarius.url": "https://nginx-aquarius.dev-ocean.com/",
-#             "brizo.url": "https://nginx-brizo.dev-ocean.com/",
-#             "storage.path": "squid_py.db",
-#             "downloads.path": "consume-downloads"
-#         }
-#     }
-#     def get_config():
-#         logging.info("Configuration loaded for environment '{}'".format(ExampleConfig.environment))
-#         return Config(options_dict=ExampleConfig.config_dict)
-# #%%
-# config_dict = {
-#     "keeper-contracts": {
-#         "keeper.url": "https://nile.dev-ocean.com",
-#         "keeper.path": "artifacts",
-#         "secret_store.url": "https://secret-store.dev-ocean.com",
-#         "parity.url": "https://nile.dev-ocean.com",
-#         "parity.address": "0x413c9ba0a05b8a600899b41b0c62dd661e689354",
-#         "parity.password": "ocean_secret",
-#         "parity.address1": "0x1322A6ef2c560107733bFc622Fe556961Cb430a5",
-#         "parity.password1": "ocean_secret"
-#     },
-#     "resources": {
-#         "aquarius.url": "https://nginx-aquarius.dev-ocean.com/",
-#         "brizo.url": "https://nginx-brizo.dev-ocean.com/",
-#         "storage.path": "squid_py.db",
-#         "downloads.path": "consume-downloads"
-#     }
-# }
-#
-#
-# config_dict2 = {
-#     "keeper-contracts" : {
-#         "keeper.url" : "https://nile.dev-ocean.com",
-#         "keeper.path" : "artifacts_nile",
-#         "secret_store.url" : "https://secret-store.dev-ocean.com",
-#         "parity.url" : "https://nile.dev-ocean.com",
-#         "parity.address" : "0x413c9BA0A05B8A600899B41b0c62dd661e689354",
-#         "parity.password" : "ocean_secret",
-#         "parity.address1" : "0x1322A6ef2c560107733bFc622Fe556961Cb430a5",
-#         "parity.password1" : "ocean_secret",
-#     },
-#     "resources" : {
-#         "aquarius.url" : "https://nginx-aquarius.dev-ocean.com/",
-#         "brizo.url" : "https://nginx-brizo.dev-ocean.com/",
-#         "storage.path" : "squid_py.db",
-#         "downloads.path" : "downloads_nile",
-#         }
-# }
 #
 #%%
 # config_from_dict = Config(options_dict=config_dict2)
@@ -129,8 +65,11 @@ def get_publisher_account(config):
 
 config = config_from_ini
 
-ConfigProvider.set_config(config)
-ocn = Ocean()
+# ConfigProvider.set_config(config)
+ocn = Ocean(config_from_ini)
+keeper = Keeper.get_instance()
+#%%
+# Get Publisher account
 acc = get_publisher_account(config)
 if not acc:
     acc = ([acc for acc in ocn.accounts.list() if acc.password] or ocn.accounts.list())[0]
@@ -139,26 +78,25 @@ ddo = ocn.assets.create(Metadata.get_example(), acc)
 logging.info(f'registered ddo: {ddo.did}')
 
 #%%
-keeper = Keeper.get_instance()
-cons_ocn = Ocean()
-# cons_ocn = Ocean(configuration)
+
+# cons_ocn = Ocean()
 consumer_account = get_account_from_config(config, 'parity.address1', 'parity.password1')
 
 #%%
 
-# sign agreement using the registered asset did above
 service = ddo.get_service(service_type=ServiceTypes.ASSET_ACCESS)
-# This will send the purchase request to Brizo which in turn will execute the agreement on-chain
-cons_ocn.accounts.request_tokens(consumer_account, 100)
+# cons_ocn.accounts.request_tokens(consumer_account, 100)
+ocn.accounts.request_tokens(consumer_account, 100)
 sa = ServiceAgreement.from_service_dict(service.as_dictionary())
 
-agreement_id = cons_ocn.assets.order(
-    ddo.did, sa.service_definition_id, consumer_account)
+agreement_id = ocn.assets.order(
+    ddo.did, 'Access', consumer_account)
 logging.info('placed order: %s, %s', ddo.did, agreement_id)
 
+#%%
 event = keeper.escrow_access_secretstore_template.subscribe_agreement_created(
     agreement_id,
-    30,
+    60,
     _log_event(keeper.escrow_access_secretstore_template.AGREEMENT_CREATED_EVENT),
     (),
     wait=True
@@ -167,7 +105,7 @@ assert event, 'no event for EscrowAccessSecretStoreTemplate.AgreementCreated'
 
 event = keeper.lock_reward_condition.subscribe_condition_fulfilled(
     agreement_id,
-    30,
+    60,
     _log_event(keeper.lock_reward_condition.FULFILLED_EVENT),
     (),
     wait=True
@@ -176,7 +114,7 @@ assert event, 'no event for LockRewardCondition.Fulfilled'
 
 event = keeper.access_secret_store_condition.subscribe_condition_fulfilled(
     agreement_id,
-    30,
+    60,
     _log_event(keeper.escrow_reward_condition.FULFILLED_EVENT),
     (),
     wait=True
@@ -185,7 +123,7 @@ assert event, 'no event for AccessSecretStoreCondition.Fulfilled'
 
 event = keeper.escrow_reward_condition.subscribe_condition_fulfilled(
     agreement_id,
-    30,
+    60,
     _log_event(keeper.escrow_reward_condition.FULFILLED_EVENT),
     (),
     wait=True
@@ -196,7 +134,7 @@ assert ocn.agreements.is_access_granted(agreement_id, ddo.did, consumer_account.
 ocn.assets.consume(
     agreement_id,
     ddo.did,
-    sa.service_definition_id,
+    'Access',
     consumer_account,
     "downloads_nile")
 logging.info('Success buying asset.')
