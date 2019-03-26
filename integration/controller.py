b981e834c2f3069c7f4af409b0ff972e39bec6d8
MarcusJones <jones.0bj3@gmail.com>
2019-03-26 10:38:49 +0100

    update schedule start stop

--- integration/controller.py
+++ integration/controller.py
@@ -1,20 +1,30 @@
 import schedule
-INTERVAL = 30
-schedule.every(INTERVAL).minutes
 import time
 import datetime
 from pathlib import Path
-PATH_TEST_FLOW = Path.cwd() / 'ipython_scripts' / '1_notebooks_blocks' / 's04_consume_asset.py'
 assert PATH_TEST_FLOW.exists(), PATH_TEST_FLOW
 import subprocess
 
+
+PATH_TEST_FLOW = Path.cwd() / 'ipython_scripts' / '1_notebooks_blocks' / 's04_consume_asset.py'
+assert PATH_TEST_FLOW.exists(), "Can't find {}".format(PATH_TEST_FLOW)
+
 def maintest():
     print("Hi",datetime.datetime.utcnow().isoformat())
-    subprocess.call(['python', PATH_TEST_FLOW])
 
-INTERVAL = 30
-schedule.every(INTERVAL).seconds.do(maintest)
+START_TIME = "10:37"
+END_TIME = "10:38"
+INTERVAL = 2
+
+def job():
+    schedule.every(INTERVAL).seconds.do(maintest)
+
+schedule.every().day.at(START_TIME).do(job)
 
 while 1:
     schedule.run_pending()
+
+
+
+
 
