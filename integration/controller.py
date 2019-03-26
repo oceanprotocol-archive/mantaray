f9610dc58e63771f04e0bfcf04b21a5e035140cf
MarcusJones <jones.0bj3@gmail.com>
2019-03-26 10:26:12 +0100

    update add schedule call to script subprocess

--- integration/controller.py
+++ integration/controller.py
@@ -3,16 +3,16 @@ INTERVAL = 30
 schedule.every(INTERVAL).minutes
 import time
 import datetime
-
-
-
-time.time()
+from pathlib import Path
+PATH_TEST_FLOW = Path.cwd() / 'ipython_scripts' / '1_notebooks_blocks' / 's04_consume_asset.py'
+assert PATH_TEST_FLOW.exists(), PATH_TEST_FLOW
+import subprocess
 
 def maintest():
     print("Hi",datetime.datetime.utcnow().isoformat())
+    subprocess.call(['python', PATH_TEST_FLOW])
 
-
-INTERVAL = 2
+INTERVAL = 30
 schedule.every(INTERVAL).seconds.do(maintest)
 
 while 1:
