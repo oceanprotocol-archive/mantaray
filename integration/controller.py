bcbbdf4f373f000b9b766d60727aa02c582a06f7
MarcusJones <jones.0bj3@gmail.com>
2019-03-27 11:13:45 +0100

    update accept environment variables

--- integration/controller.py
+++ integration/controller.py
@@ -3,22 +3,48 @@ import time
 import datetime
 from pathlib import Path
 import subprocess
+import os
 
+# Save the logs to this path
 PATH_CONTROL_LOG = Path.home() / 'Started performance test at {}.log'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+# This is the test script to run
 PATH_TEST_FLOW = Path.cwd() / 'ipython_scripts' / '1_notebooks_blocks' / 's04_consume_asset.py'
 assert PATH_TEST_FLOW.exists(), "Can't find {}".format(PATH_TEST_FLOW)
 
-START_TIME = "12:17"
-END_TIME = "20:00"
-END_HOUR = int(END_TIME.split(':')[0])
-END_MINUTE = int(END_TIME.split(':')[1])
-INTERVAL = 30
+# Get the schedule variables
+# Testing start time
+if os.environ['DOCKER_START_TIME']:
+    START_TIME = os.environ['DOCKER_START_TIME']
+elif 0: # Enable for a manual start time
+    START_TIME = "12:17"
+else: # Otherwise, just start now
+    START_TIME = datetime.datetime.now().isoformat()
 
-def job():
-    schedule.every(INTERVAL).seconds.do(maintest)
+# Testing end time
+if os.environ['DOCKER_END_TIME']:
+    END_TIME = os.environ['DOCKER_END_TIME']
+    END_HOUR = int(END_TIME.split(':')[0])
+    END_MINUTE = int(END_TIME.split(':')[1])
+else: # Enable for a manual start time
+    END_TIME = "20:00"
 
-def maintest():
+# Testing interval
+if os.environ['DOCKER_INTERVAL']:
+    INTERVAL = os.environ['DOCKER_INTERVAL']
+else:
+    INTERVAL = 30
+
+
+def schedule_at_interval():
+    """Add scheduled jobs at INTERVAL time
+    """
+    schedule.every(INTERVAL).seconds.do(run_python_test_script)
+
+
+def run_python_test_script():
+    """Simple wrapper on subprocess.call()
+    """
     log_str = "Running script {} at {}\n".format(PATH_TEST_FLOW, datetime.datetime.utcnow().isoformat())
     print(log_str, end="")
     with PATH_CONTROL_LOG.open('a') as f:
@@ -31,13 +57,12 @@ def maintest():
     print(log_str, end="")
     with PATH_CONTROL_LOG.open('a') as f:
         f.write(log_str)
-if 0:
-    # For starting at a specific time
-    schedule.every().day.at(START_TIME).do(job)
-if 1:
-    # Start immediately on process start
-    schedule.every(INTERVAL).seconds.do(maintest)
-    START_TIME = datetime.datetime.now().isoformat()
+
+
+# Schedule the jobs
+schedule.every().day.at(START_TIME).do(schedule_at_interval)
+schedule.every(INTERVAL).seconds.do(maintest)
+
 
 log_str = "Scheduler set to start at {} every {} seconds until {}\n".format(START_TIME, INTERVAL,  END_TIME)
 print("Current datetime:", datetime.datetime.now())
@@ -45,14 +70,18 @@ print(log_str, end="")
 with PATH_CONTROL_LOG.open('a') as f:
     f.write(log_str)
 
+
 while 1:
     schedule.run_pending()
+
+    # Check if END_TIME, and break
     if datetime.datetime.now().hour >= END_HOUR and datetime.datetime.now().minute >= END_MINUTE:
         log_str = "Scheduler finished at {}".format(END_TIME)
         print(log_str, end="")
         with PATH_CONTROL_LOG.open('a') as f:
             f.write(log_str)
         break
+
     time.sleep(1)
 
 
